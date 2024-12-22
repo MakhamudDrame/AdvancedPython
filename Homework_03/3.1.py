270309773291ea7ef0aaf5b7b0bc99d025f08883
MakhamudDrame <naive1899@gmail.com>
2024-12-22 18:25:24 +0300

    Add files via upload

---
+++ Homework_03/3.1.py
@@ -0,0 +1,12 @@
+from pack_math.math_sum import SumCalculator
+
+
+calculator = SumCalculator()
+
+
+numbers = [1, 2, 3, 4, 5]
+result = calculator.calculate_sum(numbers)
+
+
+print(f"Сумма чисел в списке {numbers} равна: {result}")
+
